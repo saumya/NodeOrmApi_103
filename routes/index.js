b89c03f4f3c630585d27203378c0aa0080240658
saumya <2saumyaray@gmail.com>
2019-12-21 17:16:22 +0530

    Mongo connect

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+
+//var mongoURL = appconfig.mongo.url ;
+var mongoURL = MONGODB_ADDON_URI ;
+
+mongoose.connect( mongoURL , 
+										{ useUnifiedTopology: true, 
+											useNewUrlParser: true } );
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'DB connection error:'));
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Home' });
