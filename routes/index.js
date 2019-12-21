533ad9afac1fd70f36553a66a3dd75716e230113
saumya <2saumyaray@gmail.com>
2019-12-21 21:07:58 +0530

    Bug fix

--- routes/index.js
+++ routes/index.js
@@ -39,6 +39,13 @@ router.get('/', function(req, res, next) {
   	console.log('------------ DB:Open -------------');
 
   	var kittySchema = new mongoose.Schema({ name: String });
+  	kittySchema.methods.speak = function () {
+	      var greeting = this.name
+	      ? "Meow name is " + this.name
+	      : "I don't have a name";
+	    	console.log(greeting);
+    	}
+    var Kitten = mongoose.model('Kitten', kittySchema);
   	
   	/*
 	  	var kittySchema = new mongoose.Schema({ name: String });
